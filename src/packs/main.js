fca6ecfb42a125a82f3c4541ef35a11eb67ee864
Maksim Fedotov <doutatsu@gmail.com>
2019-07-28 16:02:48 +0100

    Add Loading directive to Main pack

--- src/packs/main.js
+++ src/packs/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueAnalytics from 'vue-analytics';
+import { Loading } from 'element-ui';
 import Home from '@/views/Home.vue';
 import '@/plugins/element.js';
 import '@/stylesheets/global.scss';
@@ -9,6 +10,7 @@ import router from '@/router';
 
 Vue.config.productionTip = false;
 
+Vue.use(Loading);
 Vue.use(VueAnalytics, {
   id: 'UA-145065333-1',
   router,
