cd65ceac61576eddf1877236dc4c1b41ada06444
Maksim Fedotov <doutatsu@gmail.com>
2019-08-04 16:41:22 +0100

    Update Landing page to use tailwindcss
    This commit updates updates all properties to use
    tailwindcss utility classes

--- src/components/TheLandingPage.vue
+++ src/components/TheLandingPage.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
-  .container
-    el-card(shadow="always")
-      img.logo(src='@/assets/logo_transparent.png')
+  .container.mx-auto.w-full.h-full.flex.flex-col.justify-center.items-center
+    el-card.text-center.max-w-lg.mx-5(shadow="always" class="md:mx-0")
+      img.max-w-xs.w-full.inline.logo(src='@/assets/logo_transparent.png')
       .welcome-text.font-size-b.font-line-height-secondary
         p
           | Welcome to Kenmei, a new manga tracking website currently in
@@ -9,10 +9,13 @@
         p
           | Inspired by
           |
-          el-link(href="https://www.trackr.moe" :underline="false") trackr.moe
+          el-link.inline.align-baseline(
+            href="https://www.trackr.moe" :underline="false"
+          )
+            | trackr.moe
           |, the best manga tracking website,
           |
-          el-link(
+          el-link.inline.align-baseline(
             href="https://blog.codeanimu.net/posts/2019/07/trackr-moe-the-future/"
             :underline="false"
           )
@@ -29,7 +32,7 @@
           target='popupwindow'
           onsubmit="window.open('https://tinyletter.com/kenmei', 'popupwindow', 'scrollbars=yes,width=800,height=600');"
         )
-          el-input(
+          el-input.my-5(
             v-model="form.email"
             placeholder="Please enter your email"
             name='email'
@@ -59,44 +62,3 @@
     },
   };
 </script>
-
-<style lang="scss" scoped>
-  .container {
-    background: -webkit-linear-gradient(
-      180deg,
-      rgb(64, 158, 255),
-      rgb(217, 236, 255)
-    );
-    background: linear-gradient(
-      180deg,
-      rgb(64, 158, 255),
-      rgb(217, 236, 255)
-    );
-    height: 100%;
-    padding: 0 20px 0 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .logo {
-    max-width: 300px;
-    width: 100%;
-  }
-
-  .el-link {
-    display: inline;
-    vertical-align: baseline;
-  }
-
-  .el-card {
-    max-width: 500px;
-    text-align: center;
-  }
-
-  .el-input {
-    margin-top: 20px;
-    margin-bottom: 20px;
-  }
-</style>
