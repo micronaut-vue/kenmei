eac6228a8da790dc72cae3de7060e1250e33df02
Maksim Fedotov <doutatsu@gmail.com>
2019-08-11 14:11:59 +0100

    Add API.getMangaBulk() method
    This is a new method, for POSTing to the bulk lookup
    api endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
 
+const sanitizeManga = (manga) => {
+  const newManga = {};
+  const { title, url, latestChapter } = manga;
+
+  newManga.series = { title, url };
+  newManga.latestChapter = latestChapter;
+
+  return newManga;
+};
+
 // This can extract both the series and chapter, we want to make use of that
 export const extractSeriesID = url => url.match(/(?<=\/)\d+/g);
 
@@ -8,13 +18,15 @@ export const getManga = id => axios
   .then((response) => {
     if (response.data.error) { return {}; }
 
-    const newManga = {};
-    const { title, url, latestChapter } = response.data;
-
-    newManga.series = { title, url };
-    newManga.latestChapter = latestChapter;
-
-    return newManga;
+    return sanitizeManga(response.data);
   });
 
-export const getAllManga = () => {};
+export const getMangaBulk = ids => axios
+  .post('https://api.kenmei.co/api/v1/series/bulk', { ids })
+  .then((response) => {
+    if (response.data.error) { return []; }
+
+    return Object
+      .values(response.data.successful)
+      .map(manga => sanitizeManga(manga));
+  });
