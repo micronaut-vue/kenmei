dd6d293be29c29a52599da172b6a2a5928c95be0
Maksim Fedotov <doutatsu@gmail.com>
2019-08-18 20:00:13 +0100

    Configure tailwind
    - add custom separator, to enable pseudo classes
    - add screen definitions, including max variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   important: true,
+  separator: '_',
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      'max-sm': { max: '640px' },
+      'max-md': { max: '768px' },
+      'max-lg': { max: '1024px' },
+      'max-xl': { max: '1280px' },
+    },
     extend: {},
   },
   variants: {},
