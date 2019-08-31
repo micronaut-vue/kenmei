a529b29760da342a9d411fc4c0746e3d9454bb07
Maksim Fedotov <doutatsu@gmail.com>
2019-08-31 13:49:24 +0100

    Sanitize Manga titles to render special characters

--- tests/components/TheMangaList.spec.js
+++ tests/components/TheMangaList.spec.js
@@ -1,4 +1,6 @@
 import { mount } from '@vue/test-utils';
+import flushPromises from 'flush-promises';
+
 import MangaList from '@/components/TheMangaList.vue';
 
 describe('TheMangaList.vue', () => {
@@ -7,6 +9,7 @@ describe('TheMangaList.vue', () => {
 
     beforeEach(() => {
       mangaList = mount(MangaList, {
+        sync: false,
         propsData: {
           tableData: [
             {
@@ -28,10 +31,37 @@ describe('TheMangaList.vue', () => {
       });
     });
 
-    it(':tableData - renders rows', () => {
+    it(':tableData - renders rows', async () => {
+      await flushPromises();
+
       const table = mangaList.find('tbody');
 
       expect(table.html()).toMatchSnapshot();
+    });
+
+    it(':tableData - sanitizes manga title to convert special characters', async () => {
+      mangaList.setProps({
+        tableData: [
+          {
+            series: {
+              title: '&Uuml;bel Blatt',
+              url: 'series.example.url',
+            },
+            latestChapter: {
+              url: 'chapter.example.url',
+              info: {
+                chapter: '10',
+                title: 'Chapter Title',
+                timestamp: 1522299049,
+              },
+            },
+          },
+        ],
+      });
+
+      await flushPromises();
+
+      expect(mangaList.find('.el-link--inner').text()).toContain('Übel Blatt');
     });
   });
 });
