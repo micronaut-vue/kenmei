73ec78218abfa3b0a96784add4fb9f0afd1f6f3b
Maksim Fedotov <doutatsu@gmail.com>
2019-08-31 12:47:06 +0100

    Use Rollbar package to report client exceptions

--- src/packs/main.js
+++ src/packs/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueAnalytics from 'vue-analytics';
+import Rollbar from 'vue-rollbar';
 import { Loading } from 'element-ui';
 import Home from '@/views/Home.vue';
 import '@/plugins/element.js';
@@ -14,6 +15,22 @@ Vue.use(Loading);
 Vue.use(VueAnalytics, {
   id: 'UA-145065333-1',
   router,
+});
+Vue.use(Rollbar, {
+  accessToken: process.env.ROLLBAR_CLIENT_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  enabled: true,
+  environment: process.env.NODE_ENV,
+  payload: {
+    client: {
+      javascript: {
+        code_version: '1.0',
+        source_map_enabled: true,
+        guess_uncaught_frames: true,
+      },
+    },
+  },
 });
 
 new Vue({
