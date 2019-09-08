a4c6a217420de7910b5b02d2452a22c41f3cc7ce
Maksim Fedotov <doutatsu@gmail.com>
2019-09-08 19:09:14 +0100

    Update router
    - Refactor router to have single root path
    - Update router to have guards on protected paths
    - Show information message when guards are triggered

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import { Message } from 'element-ui';
+import store from '@/store/store';
 
 Vue.use(Router);
 
@@ -7,7 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: '/landing-page'
+      name: 'root',
+      component: () => import(/* webpackChunkName: "landing_page" */ './views/LandingPage.vue')
     },
     {
       path: '/manga-list',
@@ -15,16 +18,28 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "manga_list" */ './views/MangaList.vue')
+      component: () => import(/* webpackChunkName: "manga_list" */ './views/MangaList.vue'),
+      beforeEnter: (to, from, next) => {
+        if (store.getters["user/signedIn"]) {
+          next();
+        } else {
+          Message.error('Please sign in first');
+          next('/sign-in');
+        }
+      },
     },
     {
       path: '/sign-in',
       name: 'Sign In',
-      component: () => import(/* webpackChunkName: "sign_in" */ './views/SignOn.vue')
+      component: () => import(/* webpackChunkName: "sign_in" */ './views/SignOn.vue'),
+      beforeEnter: (to, from, next) => {
+        if (store.getters["user/signedIn"]) {
+          Message.info('You are already signed in');
+          next('/manga-list');
+        } else {
+          next();
+        }
+      },
     },
-    {
-      path: '/landing-page',
-      component: () => import(/* webpackChunkName: "landing_page" */ './views/LandingPage.vue')
-    }
   ]
 })
