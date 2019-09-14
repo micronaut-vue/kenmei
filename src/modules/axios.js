c4c815c64f4d3d3c2c46bc32734e376bc1071384
Maksim Fedotov <doutatsu@gmail.com>
2019-09-14 16:23:11 +0100

    Global axios configuration
    - setup production and dev urls
    - configure authorization headers on every request
    - handle automatic token refresh and if that fails,
    refresh the page, so the user can sign in again

---
+++ src/modules/axios.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+const DEV_API_URL = 'http://localhost:3000';
+const PRODUCTION_API_URL = 'https://api.kenmei.co';
+
+const currentEnv = () => (
+  process.env.NODE_ENV === 'production' ? PRODUCTION_API_URL : DEV_API_URL
+);
+
+const logOutFailed = response => response.config.method === 'delete'
+  && response.data.error === 'Signature has expired';
+
+const setAuthConfig = (config) => {
+  const token = localStorage.getItem('access');
+
+  if (token) { config.headers.Authorization = `Bearer ${token}`; }
+
+  return config;
+};
+
+const baseConfig = {
+  baseURL: currentEnv(),
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+const secure = axios.create(baseConfig);
+const plain  = axios.create(baseConfig);
+
+plain.interceptors.request.use(config => setAuthConfig(config));
+secure.interceptors.request.use(config => setAuthConfig(config));
+secure.interceptors.response.use(null, (error) => {
+  const { response } = error;
+  const { config, status } = response;
+
+  if (response && config && status === 401 && !logOutFailed(response)) {
+    return plain
+      .post('/refresh')
+      .then((response) => {
+        localStorage.access = response.data.access;
+
+        return plain.request(error.response.config);
+      }).catch(() => {
+        delete localStorage.access;
+        delete localStorage.vuex;
+
+        window.location.reload(true);
+      });
+  }
+  return Promise.reject(error);
+});
+
+export { secure, plain };
