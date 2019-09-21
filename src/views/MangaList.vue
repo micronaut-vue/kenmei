b4c208bf0a8fc0206e4694b11c38df7fdd2b87c3
Maksim Fedotov <doutatsu@gmail.com>
2019-09-21 16:15:02 +0100

    Update MangaList view to add/remove with new data
    - Adds new entries through the api
    - Removes entries through the api
    - Fetch lists from the api
    - Switch between different lists
    
    view

--- src/views/MangaList.vue
+++ src/views/MangaList.vue
@@ -2,8 +2,19 @@
   .container.mx-auto.w-full.h-full.flex.flex-col.items-center
     .flex.flex-col.w-full.max-w-4xl.pt-32.pb-16
       .mx-5.mb-5(class="md:mx-0")
-        el-button(
-          v-show="selectedSeries.length > 0"
+        el-select.shadow-md.rounded.float-right(
+          v-model="currentListID"
+          placeholder="Select"
+        )
+          el-option(
+            v-for="list in lists"
+            :key="list.id"
+            :label="list.attributes.name"
+            :value="list.id"
+          )
+      .mx-5.mb-5(class="md:mx-0")
+        el-button.shadow-md(
+          v-show="selectedSeriesIDs.length > 0"
           ref="removeSeriesButton"
           type="danger"
           size="medium"
@@ -12,7 +23,7 @@
         )
           i.el-icon-delete.mr-1
           | Remove
-        el-button.float-right(
+        el-button.shadow-md.float-right(
           ref="openAddMangaModalButton"
           type="primary"
           size="medium"
@@ -21,7 +32,7 @@
         )
           i.el-icon-plus.mr-1
           | Add Manga
-        el-button.float-right.mr-3(
+        el-button.shadow-md.float-right.mr-3(
           type="success"
           size="medium"
           @click="importDialogVisible = true"
@@ -30,7 +41,10 @@
           i.el-icon-upload2.mr-1
           | Import
       .flex-grow.mx-5(class="md:mx-0")
-        the-manga-list(:tableData='tableData' @seriesSelected="handleSelection")
+        the-manga-list(
+          :tableData='currentListEntries'
+          @seriesSelected="handleSelection"
+        )
       el-dialog(
         title="Import Manga List"
         :visible.sync="importDialogVisible"
@@ -84,13 +98,18 @@
 <script>
   import pLimit from 'p-limit';
   import {
-    Message, Loading, Dialog, Button, Input, Upload, Link,
+    mapActions, mapState, mapMutations, mapGetters,
+  } from 'vuex';
+  import {
+    Message, Loading, Dialog, Button, Input, Upload, Link, Select, Option,
   } from 'element-ui';
 
   import TheMangaList from '@/components/TheMangaList';
   import ProgressBar from '@/components/ProgressBar';
-  import { getManga, extractSeriesID, getMangaBulk } from '@/services/api';
-  import Importer from '@/services/importer';
+  import {
+    addMangaEntry, addMangaEntries, deleteMangaEntry, extractSeriesID,
+  } from '@/services/api';
+  import { processList, sliceIntoBatches } from '@/services/importer';
 
   export default {
     name: 'MangaList',
@@ -102,11 +121,13 @@
       'el-input': Input,
       'el-upload': Upload,
       'el-link': Link,
+      'el-select': Select,
+      'el-option': Option,
     },
     data() {
       return {
-        tableData: [],
-        selectedSeries: [],
+        selectedSeriesIDs: [],
+        currentListID: null,
         mangaURL: '',
         dialogVisible: false,
         importDialogVisible: false,
@@ -114,47 +135,59 @@
       };
     },
     computed: {
-      // TODO: Lookup by DB id instead of title when we'll start storing
-      // series on back-end
-      selectedSeriesTitles() {
-        return this.selectedSeries.map(series => series.series.title);
+      ...mapState('lists', [
+        'lists',
+      ]),
+      ...mapGetters('lists', [
+        'getEntriesByListId',
+        'entryAlreadyExists',
+      ]),
+      currentListEntries() {
+        return this.getEntriesByListId(this.currentListID);
       },
     },
+    mounted() {
+      this.retrieveLists();
+    },
     methods: {
-      handleSelection(selectedSeries) {
-        this.selectedSeries = selectedSeries;
+      ...mapActions('lists', [
+        'getLists',
+      ]),
+      ...mapMutations('lists', [
+        'addEntry',
+        'removeEntries',
+      ]),
+      handleSelection(selectedSeriesIDs) {
+        this.selectedSeriesIDs = selectedSeriesIDs;
       },
-      // TODO: When we are going to store these on the back-end, make a
-      // relevant API call
-      removeSeries() {
-        // console.log(this.selectedSeriesTitles);
-        this.tableData = this.tableData.filter(
-          item => !this.selectedSeriesTitles.includes(item.series.title)
-        );
+      async retrieveLists() {
+        await this.getLists();
+        this.currentListID = this.currentListID || this.lists[0].id;
+      },
+      async removeSeries() {
+        await this.selectedSeriesIDs.map(id => deleteMangaEntry(id));
+
+        this.removeEntries(this.selectedSeriesIDs);
       },
       mangaDexSearch() {
-        const mangaID = extractSeriesID(this.mangaURL);
-
-        if (this.alreadyExists(mangaID)) {
+        if (this.alreadyExists(this.mangaURL)) {
           Message.info('Manga already added');
           this.dialogVisible = false;
           return;
         }
 
         const loading = Loading.service({ target: '.el-dialog' });
-        getManga(mangaID)
-          .then((newManga) => {
-            if (Object.keys(newManga).length === 0) {
-              Message.info('Manga was not found');
-              loading.close();
-            } else {
-              this.tableData.push(newManga);
-              this.closeModal(loading);
-            }
+        addMangaEntry(this.mangaURL, this.currentListID)
+          .then((newMangaEntry) => {
+            this.addEntry(newMangaEntry.data);
+            this.closeModal(loading);
           })
           .catch((error) => {
             if (error.response.status === 400) {
               Message.error('URL is incorrect');
+              loading.close();
+            } else if (error.response.status === 404) {
+              Message.info('Manga was not found');
               loading.close();
             } else {
               Message.error('Something went wrong');
@@ -167,42 +200,43 @@
         loading.close();
         this.mangaURL = '';
       },
-      alreadyExists(mangaID) {
-        return this.tableData.some(manga => manga.series.url.includes(mangaID));
-      },
-      sliceIntoBatches(ids) {
-        const batchSize = 20;
-        const IDChunks = [];
-
-        for (let i = 0; i < ids.length; i += batchSize) {
-          IDChunks.push(ids.slice(i, i + batchSize).join(' '));
-        }
-
-        return IDChunks;
-      },
-      filterNewMangaDexEntries(list) {
-        return Importer(list).filter(value => !this.alreadyExists(value));
+      alreadyExists(mangaURL) {
+        // TODO: We want to save and send back MangaDex ID from back-end
+        const mangaID = extractSeriesID(mangaURL);
+        return this.entryAlreadyExists(mangaID);
       },
       processMangaDexList(list) {
-        let seriesImported = 0;
-        const promiseLimit = pLimit(1);
-        const seriesIDs = this.filterNewMangaDexEntries(list);
+        let seriesImported  = 0;
+        const filteredLists = {};
+        const promiseLimit  = pLimit(1);
+        const listsToImport = processList(list);
 
-        if (seriesIDs.length === 0) {
+        Object.entries(listsToImport).forEach(([name, list]) => {
+          filteredLists[name] = list
+            .map(url => ({
+              seriesURL: url.full_title_url,
+              lastRead: url.title_data.current_chapter,
+            }))
+            .filter(url => !this.alreadyExists(url.seriesURL));
+        });
+
+        if (Object.values(filteredLists).every(list => list.length === 0)) {
           Message.info('Nothing new to import');
           return;
         }
 
-        const IDChunks = this.sliceIntoBatches(seriesIDs);
-        const requestList = IDChunks.map(ids => promiseLimit(
-          () => getMangaBulk(ids).then(
+        const entriesToImport = Object.values(filteredLists).flat();
+        const URLChunks       = sliceIntoBatches(filteredLists);
+
+        const requestList = URLChunks.map(payload => promiseLimit(
+          () => addMangaEntries(payload).then(
             (importedList) => {
-              seriesImported += importedList.length;
+              seriesImported += importedList.data.length;
               this.importProgress = Math.floor(
-                seriesImported / seriesIDs.length * 100
+                seriesImported / entriesToImport.length * 100
               );
 
-              return importedList;
+              return importedList.data;
             }
           )
         ));
@@ -214,7 +248,6 @@
         await Promise.all(requestList)
           .then((importedList) => {
             const importedManga = importedList.flat();
-            importedManga.forEach(newManga => this.tableData.push(newManga));
 
             Message.info(`Imported ${importedManga.length} series`);
             this.importDialogVisible = false;
@@ -225,6 +258,10 @@
           .finally(() => {
             loading.close();
           });
+
+        // Request all lists again to get new lists if created
+        // TODO: Figure out based on relationships if there was a new list added
+        this.retrieveLists();
       },
       processUpload(file) {
         // Reset import progress
