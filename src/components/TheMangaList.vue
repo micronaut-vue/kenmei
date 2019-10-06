07264fd597fc5784429e0e26c73ab3aebbe3c86f
Maksim Fedotov <doutatsu@gmail.com>
2019-10-06 22:08:58 +0100

    Update styling of Manga List Component
    - Remove rounded edges and shadow if on small screens
    - Add breaks to the headers and cells

--- src/components/TheMangaList.vue
+++ src/components/TheMangaList.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  el-table.shadow-lg.rounded(
+  el-table.sm_shadow-lg.sm_rounded(
     :data="tableData"
     :default-sort = "{ prop: 'attributes.title', order: 'descending' }"
     v-loading='listsLoading'
@@ -82,3 +82,13 @@
     },
   };
 </script>
+
+<style media="screen" lang="scss">
+  .el-table th > .cell {
+    @apply break-normal;
+  }
+
+  .el-table td > .cell {
+    @apply break-normal;
+  }
+</style>
