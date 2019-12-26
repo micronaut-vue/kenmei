a37a1b1b19cf3863df2d426f5b399f7383eb7be6
Maksim Fedotov <doutatsu@gmail.com>
2019-12-26 15:10:17 +0000

    Remove SignOn view and SignOnTabs component
    These are not being used anymore, now that we just
    have a dialog for sign on

--- tests/views/SignOn.spec.js
+++
@@ -1,8 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import SignOn from '@/views/SignOn.vue';
-
-describe('SignOn.vue', () => {
-  it('renders the page', () => {
-    expect(shallowMount(SignOn).element).toMatchSnapshot();
-  });
-});
