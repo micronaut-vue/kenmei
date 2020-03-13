bec927d49a651f9b2f609acfc95784ec5057a9e6
Maksim Fedotov <doutatsu@gmail.com>
2020-03-13 12:55:21 +0000

    Add page transitions
    This applies sliding transition on all route changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
   #home.h-full.overflow-auto
     navigation-bar
-    router-view
+    transition(name="slide-left" mode="out-in")
+      router-view
 </template>
 
 <script>
@@ -14,3 +15,12 @@
     },
   };
 </script>
+
+<style media="screen" lang="scss">
+  .slide-left-enter-active, .slide-left-leave-active {
+    @apply transition ease-in duration-300 transition-all overflow-hidden;
+  }
+
+  .slide-left-enter { @apply opacity-0 transform translate-x-8; }
+  .slide-left-leave-active { @apply opacity-0 transform -translate-x-8;}
+</style>
