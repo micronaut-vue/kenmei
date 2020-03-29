6c41cc32217b26ed3830375dc04fba83d86b0da5
Maksim Fedotov <doutatsu@gmail.com>
2020-03-29 12:58:45 +0100

    Move transitions into global stylesheet
    Instead of declaring them per component, I should instead
    add them into it's own stylsheet, that we can make use in
    any component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,12 +18,3 @@
     },
   };
 </script>
-
-<style media="screen" lang="scss">
-  .slide-left-enter-active, .slide-left-leave-active {
-    @apply transition ease-in duration-300 transition-all overflow-hidden;
-  }
-
-  .slide-left-enter { @apply opacity-0 transform translate-x-8; }
-  .slide-left-leave-active { @apply opacity-0 transform -translate-x-8;}
-</style>
