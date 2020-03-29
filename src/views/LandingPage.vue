a1a574c3d6afbac872c707fc7eb34bc8197c90a0
Maksim Fedotov <doutatsu@gmail.com>
2020-03-29 12:59:39 +0100

    LandingPage uses new LandingPage components
    - Use newly created components in the landing page view

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,70 +1,64 @@
 <template lang="pug">
-  .flex.flex-col.justify-center.items-center
-    base-card.max-w-lg.text-center.mx-5.md_mx-0.my-40
-      .p-5
-        img.max-w-xs.w-full.inline.logo(src='@/assets/logo.svg' alt="logo")
-        .welcome-text.font-size-b.font-line-height-secondary
-          p
-            | Welcome to Kenmei, a new manga tracking website currently in
-            | alpha.
-          p
-            | Inspired by
-            |
-            el-link.inline.align-baseline(
-              href="https://www.trackr.moe" type="primary" :underline="false"
-            )
-              | trackr.moe
-            |, the best manga tracking website,
-            |
-            el-link.inline.align-baseline(
-              href="https://blog.codeanimu.net/posts/2019/07/trackr-moe-the-future/"
-              type="primary"
-              :underline="false"
-            )
-              | that is now slated to be closed
-            |, Kenmei is here to replace it
-            | and provide even more features, for all manga fans out there!
-          p
-            | To get development updates, please leave your email below.
-        .email-form
-          form(
-            action='https://tinyletter.com/kenmei'
-            method='post'
-            target='popupwindow'
-            onsubmit="window.open('https://tinyletter.com/kenmei', 'popupwindow', 'scrollbars=yes,width=800,height=600');"
-          )
-            el-input.my-5(
-              v-model="form.email"
-              placeholder="Please enter your email"
-              name='email'
-            )
-            input(type='hidden', value='1', name='embed')
-            input.el-button(type='submit', value='Subscribe')
+  #landing-page.font-sans
+    hero-section(@signOnOpened="openSignOnWith($event)")
+    features#features
+    stats#stats
+    call-to-action(@signOnOpened="openSignOnWith($event)")
+    base-footer
+    base-modal(
+      :visible="signOnVisible"
+      :loading="loading"
+      @dialogClosed="signOnVisible = false"
+      size="xs"
+    )
+      template(slot='body')
+        component(
+          :is='activeComponent'
+          @componentChanged='activeComponent = $event'
+          @signOnFinished='signOnVisible = false'
+          @loading='loading = $event'
+        )
 </template>
 
 <script>
-  import VueScrollTo from 'vue-scrollto';
+  import { mapGetters } from 'vuex';
 
-  import {
-    Card, Button, Input, Link,
-  } from 'element-ui';
+  import HeroSection from '@/components/landing_page/LandingPageHeroSection';
+  import Features from '@/components/landing_page/LandingPageFeatures';
+  import Stats from '@/components/landing_page/LandingPageStats';
+  import CallToAction from '@/components/landing_page/LandingPageCTA';
+  import TheSignIn from '@/components/TheSignIn';
+  import TheSignUp from '@/components/TheSignUp';
+  import TheResetPassword from '@/components/TheResetPassword';
 
   export default {
     components: {
-      'el-card': Card,
-      'el-button': Button,
-      'el-input': Input,
-      'el-link': Link,
+      HeroSection,
+      Features,
+      Stats,
+      CallToAction,
+      TheSignIn,
+      TheSignUp,
+      TheResetPassword,
     },
     data() {
       return {
-        form: {
-          email: '',
-        },
+        open: false,
+        loading: false,
+        signOnVisible: false,
+        activeComponent: 'TheSignUp',
       };
     },
-    beforeMount() {
-      VueScrollTo.scrollTo('header');
+    computed: {
+      ...mapGetters('user', [
+        'signedIn',
+      ]),
+    },
+    methods: {
+      openSignOnWith(comp) {
+        this.activeComponent = comp;
+        this.signOnVisible = true;
+      },
     },
   };
 </script>
