e376f5ad682e591d058a800a1597d13ea9ff7fd3
Maksim Fedotov <doutatsu@gmail.com>
2020-04-05 18:05:29 +0100

    Update AddMangaEntry
    Add helper text, to explain that using a chapter url
    will set latest read to that chapter

--- src/components/manga_entries/AddMangaEntry.vue
+++ src/components/manga_entries/AddMangaEntry.vue
@@ -7,17 +7,19 @@
   )
     template(slot='body')
       .mt-3.text-center.sm_mt-0.sm_text-left.w-full
-        label.block.text-sm.leading-5.font-medium.text-gray-700(for='url')
+        label.block.text-sm.font-medium.leading-5.text-gray-700(for='url')
           | Manga URL
-        .mt-1.relative.rounded-md.shadow-sm.w-auto
+        .mt-1.relative.rounded-md.shadow-sm
           .absolute.inset-y-0.left-0.pl-3.flex.items-center.pointer-events-none
-            i.el-icon-link.text-gray-400.sm_text-sm.sm_leading-5
-          input.placeholder-gray-400.focus_placeholder-gray-300(
+             i.el-icon-link.text-gray-400.sm_text-sm.sm_leading-5
+          input#email.form-input.block.w-full.pl-10.sm_text-sm.sm_leading-5(
             aria-label='Manga URL'
             name='manga_url'
             v-model.trim="mangaURL"
             placeholder='https://mangadex.org/title/7139/'
           )
+        p.mt-2.text-xs.text-gray-500
+          | When using a chapter URL, last read chapter will be pre-populated
     template(slot='actions')
       span.flex.w-full.rounded-md.shadow-sm.sm_ml-3.sm_w-auto
         base-button(
